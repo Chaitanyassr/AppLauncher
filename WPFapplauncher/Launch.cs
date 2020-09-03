464fb4482f9df109850f72ebfee3d30ec76fa3d3
Chaitanyassr <chaitanyas0101@gmail.com>
2020-09-04 00:34:19 +0530

    added proceess

--- WPFapplauncher/Launch.cs
+++ WPFapplauncher/Launch.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace WPFapplauncher
 {
     class Launch
     {
+        public void PlayGame()
+        {
+            Process.Start("app.exe");
+        }
     }
 }
